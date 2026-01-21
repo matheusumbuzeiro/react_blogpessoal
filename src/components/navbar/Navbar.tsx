73827aff4e4b023e02f6741fb3eff35619dbe74a
matheusumbuzeiro <matheusumbuzeiro@outlook.com>
2026-01-20 23:14:58 -0300

    Adicionando CardTema, Lista Temas e Deletar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,33 +1,34 @@
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
-import { useContext } from "react";
 
 function Navbar() {
 
-    const navigate = useNavigate ();
+    const navigate = useNavigate();
 
-    const {handleLogout} = useContext(AuthContext)
+    const { handleLogout } = useContext(AuthContext)
 
-    function logout () {
+    function logout() {
+
         handleLogout()
-        alert('Usuário foi desconectado com sucesso!')
+        alert('O Usuário foi desconectado com sucesso!')
         navigate('/')
     }
 
     return (
         <>
             <div className='w-full flex justify-center py-4
-                            bg-indigo-900 text-white'>
-
+            			   bg-indigo-900 text-white'>
+            
                 <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
                         Postagens
-                        Temas
-                        Cadastrar tema
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
-                        <Link to='' onClick={logout} className= 'hover:underline'>Sair</Link>
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
